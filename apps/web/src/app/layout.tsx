16ccefd8fcaac57ce88d2611c73226b0c4ebd7ae
Parker Wahle <regulad@regulad.xyz>
2024-03-16 18:44:42 -0400

    socratalk

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -4,17 +4,17 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/react";
 import { type ReactNode } from "react";
 import ContextProvider from "./lib/context";
+// import "@repo/ui/styles.css";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    template: "%s | Socratic Showdown",
-    default: "Socratic Showdown",
+    template: "%s | SocraTalk",
+    default: "SocraTalk",
   },
-  description:
-    "Socratic Showdown gamifies the Socratic Seminar to provide a more engaging and interactive experience for students.",
+  description: "Gamify your classroom discussions",
 };
 
 export default function RootLayout({
